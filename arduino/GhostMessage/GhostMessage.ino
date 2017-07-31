c6f8a9ca8c5e1380198074efb7b0c80283886e07
Aaron Eiche <aaron@aaroneiche.com>
2017-07-30 17:29:27 -0700

    Initial commit. Includes Arduino Code, web server, and page.
    Signed-off-by: Aaron Eiche <aaron@aaroneiche.com>

---
+++ arduino/GhostMessage/GhostMessage.ino
@@ -0,0 +1,178 @@
+// testshapes demo for Adafruit RGBmatrixPanel library.
+// Demonstrates the drawing abilities of the RGBmatrixPanel library.
+// For 16x32 RGB LED matrix:
+// http://www.adafruit.com/products/420
+
+// Written by Limor Fried/Ladyada & Phil Burgess/PaintYourDragon
+// for Adafruit Industries.
+// BSD license, all text above must be included in any redistribution.
+
+#include <Adafruit_GFX.h>   // Core graphics library
+#include <RGBmatrixPanel.h> // Hardware-specific library
+
+#define CLK 8  // MUST be on PORTB! (Use pin 11 on Mega)
+#define LAT 10
+#define OE  9
+#define A   A0
+#define B   A1
+#define C   A2
+RGBmatrixPanel matrix(A, B, C, CLK, LAT, OE, false);
+
+const unsigned char PROGMEM ghost[] = 
+{
+B00000111,B10000000,
+B00011111,B11100000,
+B00111111,B11110000,
+B01111111,B11111000,
+B01111111,B11111000,
+B01111111,B11111000,
+B01111111,B11111000,
+B11111111,B11111100,
+B11111111,B11111100,
+B11111111,B11111100,
+B11111111,B11111100,
+B11111111,B11111100,
+};
+
+const unsigned char PROGMEM skirtA[] =
+{
+B11011100,B11101100,
+B10001100,B11000100
+};
+
+const unsigned char PROGMEM skirtB[] =
+{
+B11110111,B10111100,
+B01100011,B00011000
+};
+
+
+const unsigned char PROGMEM eyeWhite[] = {
+  B01100000,
+  B11110000,
+  B11110000,
+  B11110000,
+  B01100000
+};
+
+const unsigned char PROGMEM pupil[] = {
+  B11000000,
+  B11000000
+};
+
+//const uint16_t BLUE = matrix.Color444(0,0,120);
+const uint16_t WHITE = matrix.ColorHSV(0, 0, 90, true);
+const uint16_t RED = matrix.ColorHSV(0, 100, 100, true);
+
+const uint16_t GREEN = 0x0080; //matrix.ColorHSV(0, 120, 90, true);
+const uint16_t BLUE = 0x0002;
+
+
+bool toggle = true;
+bool dir = true;
+int ghostx = -14;
+int ghosty = 1;
+int messageWidth = 0;
+
+unsigned long previousMillis = 0;
+long interval = 10000;
+
+String lastMessage = "";
+String sentMessage = "";
+
+
+void drawGhost(int x, int y, uint16_t color, bool direction, bool skirt){
+  
+  matrix.drawBitmap(x,y,ghost,14,14,color);
+
+  if(direction){
+    matrix.drawBitmap(x+3,y+3,eyeWhite,4,5,WHITE);
+    matrix.drawBitmap(x+5,y+5,pupil,2,2,BLUE);
+    
+    matrix.drawBitmap(x+9,y+3,eyeWhite,4,5,WHITE);
+    matrix.drawBitmap(x+11,y+5,pupil,2,2,BLUE);
+  }else{
+    matrix.drawBitmap(x+1,y+3,eyeWhite,4,5,WHITE);
+    matrix.drawBitmap(x+1,y+5,pupil,2,2,BLUE);
+    
+    matrix.drawBitmap(x+7,y+3,eyeWhite,4,5,WHITE);
+    matrix.drawBitmap(x+7,y+5,pupil,2,2,BLUE);    
+  }
+
+
+  if(skirt){
+    matrix.drawBitmap(x,y+12,skirtA,14,2,color);
+  }else{
+    matrix.drawBitmap(x,y+12,skirtB,14,2,color);
+  }
+}
+
+
+void drawMessage(int x, int y, String message){ //char *message
+  matrix.setCursor(x, y);
+  matrix.setTextColor(GREEN);
+  matrix.print(message);
+}
+
+void setup() {
+  Serial.begin(9600);
+  matrix.begin();
+  matrix.setTextWrap(false);
+  matrix.setTextSize(1);
+  
+  matrix.setTextColor(WHITE);
+
+  matrix.setCursor(0,0);
+  matrix.print("Ghost");
+  matrix.setCursor(0,9);
+  matrix.print("Message");
+  drawGhost(18,ghosty,GREEN,false,true);
+  
+  delay(4000);
+  matrix.fillScreen(0);
+  
+}
+
+void loop() {
+
+  unsigned long currentMillis = millis();
+
+  while (Serial.available() > 0) {
+    sentMessage = Serial.readString();
+    Serial.print("received:");
+    Serial.println(sentMessage);
+  }
+
+  if(sentMessage != lastMessage){
+    
+    messageWidth = sentMessage.length() * -6;
+    matrix.fillScreen(0);
+    toggle = (toggle) ? false: true;
+    drawGhost(ghostx,ghosty,RED,dir,toggle);
+  
+    if(dir == true){
+      ghostx++;
+    }else if(dir == false){
+      ghostx--;
+    }
+  
+    if(!dir){
+      drawMessage(ghostx + 15, 4, sentMessage);
+    }
+
+    if(ghostx > matrix.width() && dir == true){
+      dir = false;
+    }else if(ghostx < messageWidth - 14 - matrix.width() && dir == false) {
+      dir = true;
+      ghostx = -14;
+      lastMessage = sentMessage;
+    }
+    delay(50);
+  }
+
+
+  if(previousMillis + interval > currentMillis){
+    
+  }
+
+}
---
+++ index.js
@@ -0,0 +1,65 @@
+// 'use strict';
+
+var webport = 8080;
+// var portDevice = '/dev/ttyUSB1';
+var portDevice;
+
+if(process.argv.length > 2){
+  portDevice = process.argv[2];
+}else{
+  portDevice = '/dev/ttyS0';
+}
+console.log("Opening port on " + portDevice);
+const SerialPort = require('serialport');
+const port = new SerialPort(portDevice);
+const express= require('express');
+const WebSocket = require('ws');
+const randomId = require('random-id');
+
+var bodyParser = require('body-parser');
+var app = express();
+
+app.use(express.static('public'));
+app.use(bodyParser.json());
+
+var server = require('http').createServer(app);
+
+server.listen(webport,function(){
+  console.log("Web Server listening on port " + webport);
+});
+
+var wsServer = new WebSocket.Server({server});
+var wsConnections = {};
+
+port.on('open', () => {
+  console.log('Connected to Serial Port.');
+});
+
+app.post("/message", function(req,res,next){
+
+  var m = req.body;
+
+  port.write(m.message);
+  res.send("Message Sent!");
+  wsServer.broadcast(JSON.stringify(m));
+
+})
+
+wsServer.broadcast = function broadcast(data) {
+  wsServer.clients.forEach(function each(client) {
+    if (client.readyState === WebSocket.OPEN) {
+      client.send(data);
+    }
+  });
+};
+
+
+wsServer.on('connection',(ws,req)=>{
+  console.log("User Connected: ", ws._socket.remoteAddress);
+})
+
+wsServer.on('close',function(ws,req){
+  console.log("socket close");
+  console.log(ws)
+  connections.splice(connections.indexOf(ws));
+})
